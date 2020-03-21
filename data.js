53c7e21f2e407f3e8ba01932c364da4e50b84c01
Dylan Vandenberg <56117712+Dylanahs@users.noreply.github.com>
2020-03-21 16:40:54 +0100

    Indienen Opdracht 2
    Indienen Opdracht 2

--- data.js
+++ data.js
@@ -62,7 +62,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/261c3938-9473-4f41-68b1-8a0444fe320c-502d/",
                 "target": "_blank"
             }
         ]
